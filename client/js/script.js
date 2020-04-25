92b6ad03bd7f65847f0ec25842ca9c649f867337
Riley Lannon <lannon.riley@gmail.com>
2020-04-25 15:55:24 -0500

    Boilerplate client code

---
+++ client/js/script.js
@@ -0,0 +1,27 @@
+/*
+
+Cocktails
+script.js
+Copyright 2020 Riley Lannon
+
+Basic scripts for the cocktail tool
+api_access.js is included before this file, so we can utilize its functions here
+
+
+*/
+
+function display_name_query() {
+    // todo: show name query box
+}
+
+function display_ingredients_query() {
+    // todo: display name query
+}
+
+function display_drinkware_query() {
+    // todo: display drinkware query
+}
+
+function display_garnish_query() {
+    // todo: display garnish query
+}
